f854216a1d18d3308d424196d3514621a630aa23
sixthson6 <sudais2408@gmail.com>
2025-06-03 15:07:48 -0700

    Add Model Repositories

---
+++ src/main/java/dev/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package dev.repository;
+
+import dev.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
+
+
